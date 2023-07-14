e7b51b8af01eb61470189cd7b3b5ccde14dc99f4
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-07-15 01:27:01 +0300

    Implemented create operation

--- src/index.js
+++ src/index.js
@@ -3,18 +3,33 @@ import './icons.css';
 import './style.css';
 
 const tasks = new Tasks();
+const form = document.querySelector('.section-form');
 const list = document.querySelector('.section-list');
 
 const fragment = new DocumentFragment();
 
-list.innerHTML = '';
-tasks.items.forEach((task, index) => {
-  task.index = index;
-  const item = document.createElement('li');
-  let html = `<input type="checkbox" value="${task.index}"/>`;
-  html += `<input type="text" value="${task.description}" readonly/>`;
-  html += '<button type="button" class="icon-drag"></button>';
-  item.innerHTML = html;
-  fragment.appendChild(item);
-});
-list.appendChild(fragment);
+const displayList = () => {
+  list.innerHTML = '';
+  tasks.items.forEach((task, index) => {
+    task.index = index;
+    const item = document.createElement('li');
+    let html = `<input type="checkbox" value="${task.index}"/>`;
+    html += `<input type="text" value="${task.description}" readonly/>`;
+    html += '<button type="button" class="icon-drag"></button>';
+    item.innerHTML = html;
+    fragment.appendChild(item);
+  });
+  list.appendChild(fragment);
+};
+
+const formEvent = (event) => {
+  event.preventDefault();
+  if (form.description.value) {
+    tasks.add(form.description.value.trim());
+    form.reset();
+    displayList();
+  }
+};
+
+form.addEventListener('submit', formEvent);
+displayList();

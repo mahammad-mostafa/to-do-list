9a72f3258914880effc2b07484009078c089744f
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-07-14 15:43:42 +0300

    Created basic webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const Path = require('path');
+const Html = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    clean: true,
+    filename: 'main.js',
+    path: Path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [{ test: /\.css$/i, use: ['style-loader', 'css-loader'] }],
+  },
+  plugin: [new Html({ template: './src/index.html' })],
+};

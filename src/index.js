018c043fd2a74d2f4b78c0b3875be5f540ff0b08
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-07-15 01:05:45 +0300

    Imported list class and applied read operation

--- src/index.js
+++ src/index.js
@@ -1,30 +1,17 @@
+import Tasks from './list.js';
 import './icons.css';
 import './style.css';
 
-const tasks = [
-  {
-    index: 1,
-    completed: false,
-    description: 'Buy gorcery',
-  },
-  {
-    index: 2,
-    completed: false,
-    description: 'Clean house',
-  },
-  {
-    index: 3,
-    completed: false,
-    description: 'Repair car',
-  },
-];
-
+const tasks = new Tasks();
 const list = document.querySelector('.section-list');
 
 const fragment = new DocumentFragment();
-tasks.forEach((task) => {
+
+list.innerHTML = '';
+tasks.items.forEach((task, index) => {
+  task.index = index;
   const item = document.createElement('li');
-  let html = '<input type="checkbox" value="1"/>';
+  let html = `<input type="checkbox" value="${task.index}"/>`;
   html += `<input type="text" value="${task.description}" readonly/>`;
   html += '<button type="button" class="icon-drag"></button>';
   item.innerHTML = html;

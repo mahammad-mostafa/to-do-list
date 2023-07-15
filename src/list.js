a9742adbc3f1d2c480f95fd35592680cf7e75b43
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-07-15 04:04:56 +0300

    Created update & complete methods

--- src/list.js
+++ src/list.js
@@ -6,13 +6,38 @@ export default class List {
   }
 
   add = (description) => {
-    const task = new Task(description);
+    const task = new Task(this.items.length + 1, description);
     this.items.push(task);
     this.store();
   }
 
   remove = (index) => {
     this.items = this.items.filter((item) => item.index !== index);
+    for (let i = index - 1; i < this.items.length; i += 1) {
+      this.items[i].index -= 1;
+    }
+    this.store();
+  }
+
+  update = (index, description) => {
+    this.items.find((item, position) => {
+      if (item.index === index) {
+        this.items[position].description = description;
+        return true;
+      }
+      return false;
+    });
+    this.store();
+  }
+
+  complete = (index) => {
+    this.items.find((item, position) => {
+      if (item.index === index) {
+        this.items[position].completed = !this.items[position].completed;
+        return true;
+      }
+      return false;
+    });
     this.store();
   }
 

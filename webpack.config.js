3ad20833d1cdb4c832979e5847b760709b930adb
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-07-16 03:04:40 +0300

    Set mode to production & kept assets filenames

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,13 @@ const Path = require('path');
 const Html = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     clean: true,
     filename: 'main.js',
     path: Path.resolve(__dirname, './dist'),
+    assetModuleFilename: '[name][ext]',
   },
   module: {
     rules: [{ test: /\.css$/i, use: ['style-loader', 'css-loader'] }, { test: /\.ttf$/i, type: 'asset/resource' }],
